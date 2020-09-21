8d0581fde6f5dfee0e15409da55745a39c5b7053
Richard Perez <PEREZR27@my.erau.edu>
2020-09-21 09:55:22 -0500

    Here are my Generics!

--- GenericCollections/Generics.cs
+++ GenericCollections/Generics.cs
@@ -11,7 +11,7 @@ namespace GenericCollections
             {
                 listOne.Add(num);
             }
-            Console.WriteLine("1. List");
+            Console.WriteLine("1. List \n");
             for (int i = 0; i < listOne.Count; i++)
             {
                 int num = listOne[i];
@@ -22,7 +22,7 @@ namespace GenericCollections
         private void linkedMethod()
         {
             LinkedList<string> listvs = new LinkedList<string>();
-            Console.WriteLine("2. Linked List");
+            Console.WriteLine("2. Linked List \n");
             foreach (string one in new string[5] { "First", "Second", "Third", "Fourth", "Fifth" })
             {
                 listvs.AddFirst(one);
@@ -37,7 +37,7 @@ namespace GenericCollections
         private void queueMethod()
         {
             Queue<int> queueOne = new Queue<int>();
-            Console.WriteLine("3. Queue");
+            Console.WriteLine("3. Queue \n");
             foreach (int num in new int[5] { 1, 2, 3, 4, 5 })
             {
                 queueOne.Enqueue(num);
@@ -47,7 +47,7 @@ namespace GenericCollections
         }
         private void stackMethod()
         {
-            Console.WriteLine("4. Stack");
+            Console.WriteLine("4. Stack \n");
             Stack<string> stackOne = new Stack<string>();
             foreach (var stack in new string[5] { "1st", "2nd", "3rd", "4th", "5th" })
             {
@@ -62,7 +62,7 @@ namespace GenericCollections
         }
         private void dictionaryMethod()
         {
-            Console.WriteLine("5. Dictionary");
+            Console.WriteLine("5. Dictionary\n");
             Dictionary<string, int> dictOne = new Dictionary<string, int>();
             dictOne.Add("First", 1);
             dictOne.Add("Second", 2);
@@ -79,7 +79,7 @@ namespace GenericCollections
         }
         private void sortMethod()
         {
-            Console.WriteLine("6. Sorted List");
+            Console.WriteLine("6. Sorted List\n");
             SortedList<int, string> sortOne = new SortedList<int, string>();
             sortOne.Add(1, "First");
             sortOne.Add(2, "Second");
@@ -103,7 +103,7 @@ namespace GenericCollections
                 , "4"
                 , "5"
             });
-            Console.WriteLine("7. HashSet");
+            Console.WriteLine("7. HashSet\n");
             foreach (var hope in hashOne)
             {
                 Console.WriteLine(hope);
